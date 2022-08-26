d1b3aea1a54bf68d790143cb666c5ba06bc129a7
chanberg <chantal.amrhein@bluewin.ch>
2022-08-26 20:33:42 +0200

    allow table printing

--- breakit/cli/evaluate.py
+++ breakit/cli/evaluate.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import re
 from typing import List
 
 from jsonargparse import ArgumentParser
@@ -22,16 +23,23 @@ def get_arg_parser() -> ArgumentParser:
                         nargs='+',
                         required=True,
                         help='Input TSV files with sources, translation hypotheses, reference translations and metric scores.')
+
+    parser.add_argument('-p', '--pretty_print',
+                        action='store_true',
+                        help='If set will print readable format, otherwise will print format that can be copied to spreadsheet.')
     return parser
 
 
 def comp_corr(good_scores: List[float],
               incorrect_scores: List[float]) -> float:
     '''
-    Compute correlation as Kendall Tau scores.
+    Compute correlation as Kendall Tau-like scores.
     '''
+    if good_scores.isnull().values.any() or incorrect_scores.isnull().values.any():
+        return np.nan
+
     concordant = np.sum(good_scores > incorrect_scores)
-    discordant = np.sum(good_scores <= incorrect_scores)
+    discordant = np.sum(good_scores < incorrect_scores)
 
     tau = (concordant - discordant) / (concordant + discordant)
 
@@ -47,16 +55,27 @@ def evaluate() -> None:
     for f in cfg.inputs:
         tsv_f = read_file(f)
 
-        metrics = [m.split('-')[0] for m in tsv_f.columns if m.endswith('-good')]
+        metrics = [re.sub('-good', '', m) for m in tsv_f.columns if m.endswith('-good')]
 
         # Compute Kendall Tau grouped by phenomenon and metric
-        print(f'\n\nEvaluating {f}')
+        if cfg.pretty_print:
+            print(f'\n\nEvaluating {f}')
         phenomena_dfs = [(l, df) for l, df in tsv_f.groupby(['phenomena'])]
+
+        if not cfg.pretty_print:
+            print('phenomenon\t'+'\t'.join(metrics))
+
         for label, p in phenomena_dfs:
-            print(f'\n{label}\t{len(p)}')
+            results = []
+            if cfg.pretty_print:
+                print(f'\n{label}\t{len(p)}')
             for m in metrics:
                 tau = comp_corr(p[f'{m}-good'], p[f'{m}-bad'])
-                print(f'\t{m}\t{tau}')
+                results.append(str(tau))
+                if cfg.pretty_print:
+                    print(f'\t{m}\t{tau}')
+            if not cfg.pretty_print:
+                print(label+'\t'+'\t'.join(results))
 
 
 if __name__ == '__main__':
